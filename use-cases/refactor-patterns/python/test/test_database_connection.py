5517101eb40e433749cb2daa831c8155692b0508
Siphephile Sithole <siphephilesithole54@gmail.com>
2026-02-19 09:27:08 +0200

    Update test_database_connection.py
    Signed-off-by: Siphephile Sithole <siphephilesithole54@gmail.com>

--- use-cases/refactor-patterns/python/test/test_database_connection.py
+++ use-cases/refactor-patterns/python/test/test_database_connection.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 from io import StringIO
 
 # Import the module to test
-sys.path.append('.')
+sys.path.append('src')
 from database_connection import DatabaseConnectionFactory
 
 class TestDatabaseConnectionBase(unittest.TestCase):
