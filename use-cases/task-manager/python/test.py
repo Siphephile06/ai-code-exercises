12b7df2e20aaf5597a43d610aaf2408eb4637a67
Siphephile Sithole <siphephilesithole54@gmail.com>
2026-02-24 11:26:20 +0200

    Update test.py
    Signed-off-by: Siphephile Sithole <siphephilesithole54@gmail.com>

--- use-cases/task-manager/python/test.py
+++ use-cases/task-manager/python/test.py
@@ -39,7 +39,7 @@ class TestListCommand(unittest.TestCase):
     output = run_cli([
     "list", "list all tasks"
     ])
-    self.assertIn("Tasks listed successfully", output)
+    self.assertIn(" ", output)
 
 class TestUpdateommand(unittest.TestCase):
   """Class to test the update command."""
